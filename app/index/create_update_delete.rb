729b57aacb4d78f6910753833887c738b98b755e
shidash <shidash@shidash.com>
2017-05-23 11:26:55 -0400

    Changed everything to use bulk indexing

--- app/index/create_update_delete.rb
+++ app/index/create_update_delete.rb
@@ -7,10 +7,9 @@ module CreateUpdateDelete
   # Index an array of items
   def create_items(items, index_name, item_type)
     doc_class = get_model(index_name, item_type)
-    create_bulk_items(items, index_name, doc_class, item_type)
-   # items.each do |item|
-  #    create_item(item, index_name, doc_class, item_type)
-  #  end
+    items.each_slice(3000).each do |item_slice|
+      create_bulk_items(item_slice, index_name, doc_class, item_type)
+    end
   end
 
   # Process the fields in the document before creating
@@ -23,17 +22,18 @@ module CreateUpdateDelete
 
     # Add date and track versions
     processed_doc_data = remap_dates(index_name, item_type, doc_data).merge({id: id, thread_id: thread_id})
-    return track_versions(processed_doc_data, doc_class, datasource)
+    return track_versions(processed_doc_data, doc_class, datasource).merge({id: id, thread_id: thread_id})
   end
 
   # Create items in bulk
   def create_bulk_items(items, index_name, doc_class, item_type)
      # Remap the items before indexing them in bulk        
-     remapped_items = items.map do |item| 
-       {index: {_id: item["id"], 
-                data: process_doc_fields(item, index_name, doc_class, item_type)}}
+    remapped_items = items.map do |item|
+       processed_fields = process_doc_fields(item, index_name, doc_class, item_type)
+       {index: {_id: processed_fields[:id], 
+                data: processed_fields}}
      end
-
+     
      # Index the documents in bulk
      Elasticsearch::Model.client = Elasticsearch::Client.new log: true, 
                                                              request_timeout: 5*60
@@ -44,21 +44,5 @@ module CreateUpdateDelete
      rescue
        binding.pry
      end
-  end
-  
-  # Index a new item
-  def create_item(doc_data, index_name, doc_class, item_type)
-    processed_doc = process_doc_fields(doc_data, index_name, doc_class, item_type)
-
-    # Create the doc or stop if it fails
-    begin
-      if doc_class.exists?(doc_data[:id])                              
-        doc_class.find(doc_data[:id]).update(version_tracked, index: index_name)
-      else
-        doc_class.create version_tracked, index: index_name
-      end
-    rescue
-      binding.pry
-    end
   end
 end
