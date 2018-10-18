ddbe4cb289d8d64f7e992c540464da0b2c56b002
M C McGrath <shidash@shidash.com>
2018-10-17 23:46:34 -0400

    Fixed bug with doc count (due to index name being processed incorrectly)

--- app/queries/query_builder.rb
+++ app/queries/query_builder.rb
@@ -90,7 +90,7 @@ module QueryBuilder
 
   # Filter the models to check
   def filter_models_to_check(doc_type)
-    type_class = "GenerateDocModel::"+@index_name.capitalize+doc_type
+    type_class = "GenerateDocModel::"+@index_name.camelize+doc_type
     return @models.select{|m| m.to_s == type_class}
   end
 end
